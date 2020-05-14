d2756862f4f92479ab506b422d07c0bf92e1c42a
novijook <m.vanroosmalen@noviuniversity.com>
2020-05-14 22:25:58 +0200

    commit FoodDialog select food

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { GlobalStyle } from "./Styles/GlobalStyle";
 import { Navbar } from "./Navbar/Navbar";
 import { Banner } from "./Banner/Banner";
+import { FoodDialog } from "./FoodDialog/FoodDialog";
 import { Menu } from "./Menu/Menu";
 
 function App() {
+  const [openFood, setOpenFood] = useState();
   return (
     <>
       <GlobalStyle />
+      <FoodDialog openFood={openFood} setOpenFood={setOpenFood} />
       <Navbar />
       <Banner />
-      <Menu />
+      <Menu setOpenFood={setOpenFood} />
     </>
   );
 }
