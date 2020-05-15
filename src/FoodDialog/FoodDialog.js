c06253615bf71ce638a00f56d1c84351e1f48e8c
novijook <m.vanroosmalen@noviuniversity.com>
2020-05-15 22:36:18 +0200

    commit price

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { FoodLabel } from "../Menu/FoodGrid";
 import { pizzaRed } from "../Styles/colors";
 import { title } from "../Styles/title";
+import { formatPrice } from "../Data/FoodData";
 
 const Dialog = styled.div`
   width: 500px;
@@ -87,7 +88,9 @@ export function FoodDialog({ openFood, setOpenFood, setOrders, orders }) {
         </DialogBanner>
         <DialogContent></DialogContent>
         <DialogFooter>
-          <ConfirmButton onClick={addToOrder}>Add to order</ConfirmButton>
+          <ConfirmButton onClick={addToOrder}>
+            Add to order: {formatPrice(openFood.price)}
+          </ConfirmButton>
         </DialogFooter>
       </Dialog>
     </>
